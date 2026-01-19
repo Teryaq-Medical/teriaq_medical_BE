7d7c6ecff52133cb96b3a4086549a7e115706322
saalehmohamedd <saalehmohamedd@gmail.com>
2026-01-19 22:38:41 +0200

    Add collected static for Vercel

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
-pip install -r requirements.txt
-python3.x manage.py collectstatic --no-input --clear
+#!/bin/bash
+
+echo "Installing dependencies..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Running migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3.9 manage.py collectstatic --noinput --clear
