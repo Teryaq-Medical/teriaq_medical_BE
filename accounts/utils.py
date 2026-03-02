40a156fc50ff93de3749823f6670cf5b16660a22
saalehmohamedd <saalehmohamedd@gmail.com>
2026-03-02 19:44:08 +0200

    updated cookies settings2

--- accounts/utils.py
+++ accounts/utils.py
@@ -5,7 +5,7 @@ def set_auth_cookie(response, token_key):
         value=token_key,
         httponly=True,
         # In production (DEBUG=False), these must be True and "None"
-        secure=not settings.DEBUG, 
-        samesite="Lax" if settings.DEBUG else "None",
+        secure=True, 
+        samesite="None",
         max_age=60 * 60 * 24 * 7 # 7 days
     )
