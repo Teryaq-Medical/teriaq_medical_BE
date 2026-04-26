67b5f20288c67434a044a6030e345726ee6e1458
saalehmohamedd <saalehmohamedd@gmail.com>
2026-04-26 17:51:21 +0300

    fixed/doctors.views

--- doctors/views.py
+++ doctors/views.py
@@ -2,7 +2,7 @@ import logging
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser,SAFE_METHODS
 from django.contrib.contenttypes.models import ContentType
 
 from .models import Doctor, WorkSchedule, DoctorAssignment, UnregisteredDoctor
